18368993e30781f05dbc45d49758e7809536725e
Georgii Vlasov <honest.niceman@gmail.com>
2022-04-18 20:44:52 +0400

    ProductRepository query by vendor name has been added

--- src/main/java/jpa/buddy/stockmanagement/repositories/ProductRepository.java
+++ src/main/java/jpa/buddy/stockmanagement/repositories/ProductRepository.java
@@ -5,9 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     @Query("select count(p) from Product p where upper(p.productType.name) = upper(?1)")
     long countByProductType(String name);
+
+    @Query("select p from Product p " +
+            "where p.vendor.name = :name " +
+            "order by p.name")
+    List<Product> findByVendorName(@Param("name") String name);
 }
